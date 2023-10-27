9d14a540d0237526ae8e530d256a3f2cc6049083
Roiney Beal <nei_beal@hotmail.com>
2023-10-27 15:55:25 -0300

    teste

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:20.5.1-slim
 
-USER node
+USER root
 
 WORKDIR /home/node/app
 
 CMD [ "tail", "-f", "/dev/null" ]
+
--- src/domain/_tests__/category.entity.spec.ts
+++ src/domain/_tests__/category.entity.spec.ts
@@ -1,3 +1,26 @@
-test("xpt", () => {
-  expect(1 + 2).toBe(3);
+import { Category } from "../category.entity";
+
+describe("Category Unit Testes", () => {
+  test("constructor", () => {
+    const category = new Category({ name: "Movie" });
+    expect(category.category_id).toBeUndefined();
+    expect(category.name).toBe("Movie");
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeTruthy();
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+
+  const created_at = new Date();
+  const category = new Category({
+    name: "Movie",
+    description: "Movie description",
+    is_active: false,
+    created_at,
+  });
+
+  expect(category.category_id).toBeUndefined();
+  expect(category.name).toBe("Movie");
+  expect(category.description).toBe("Movie description");
+  expect(category.is_active).toBeFalsy();
+  expect(category.created_at).toBeInstanceOf(Date);
 });
